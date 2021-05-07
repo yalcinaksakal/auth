9ded23a0ebd988dfe16b5f3689450af35bbc0b18
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-05-07 03:25:29 +0300

    auth

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import { Switch, Route } from 'react-router-dom';
+import { useContext } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import AuthContext from "./store/auth-context";
 
 function App() {
+  const { isLoggedIn } = useContext(AuthContext);
   return (
     <Layout>
       <Switch>
-        <Route path='/' exact>
-          <HomePage />
+        <Route path="/" exact>
+          {isLoggedIn ? <HomePage /> : <Redirect to="/auth" />}
         </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
+
+        {!isLoggedIn && (
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+        )}
+        {isLoggedIn && (
+          <Route path="/profile">
+            <UserProfile />
+          </Route>
+        )}
+        <Route path="*">
+          <Redirect to="/auth" />
         </Route>
       </Switch>
     </Layout>
--- src/components/StartingPage/StartingPageContent.js
+++ src/components/StartingPage/StartingPageContent.js
@@ -1,9 +1,12 @@
-import classes from './StartingPageContent.module.css';
+import { useContext } from "react";
+import AuthContext from "../../store/auth-context";
+import classes from "./StartingPageContent.module.css";
 
 const StartingPageContent = () => {
+  const { isLoggedIn } = useContext(AuthContext);
   return (
     <section className={classes.starting}>
-      <h1>Welcome on Board!</h1>
+      <h1>{isLoggedIn ? "Welcome on Board!" : "Please Login"}</h1>
     </section>
   );
 };
